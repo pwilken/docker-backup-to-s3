0db2976f4aaa3a7fd360d1b20ad660cf866cf340
Pierre Wilken <pierre.wilken@impect.com>
2019-11-20 18:47:49 +0100

    :boom: :fire: :hammer: just basics for sync to s3

--- sync.sh
+++ sync.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+
+if [[ -n "$ACCESS_KEY"  &&  -n "$SECRET_KEY" ]]; then
+    echo "access_key=$ACCESS_KEY" >> /root/.s3cfg
+    echo "secret_key=$SECRET_KEY" >> /root/.s3cfg
+else
+    echo "No ACCESS_KEY and SECRET_KEY env variable found, assume use of IAM"
+    exit 1
+fi
+
 echo "Job started: $(date)"
 
 /usr/local/bin/s3cmd sync $PARAMS "$DATA_PATH" "$S3_PATH"
